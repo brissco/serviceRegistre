59d548ea0a722458ca83d2e277e16ae80a26c246
brissco <ngatchabrice03@gmail.com>
2023-05-12 21:16:19 +0100

    first push

---
+++ src/test/java/com/service/registre/ServiceRegistreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.registre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
